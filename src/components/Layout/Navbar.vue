26974fadf6434ca6b1098089f0083e397af27b83
David <mac@macs-MacBook-Air.local>
2025-02-10 13:41:13 +0100

    create a new route-link for about page

--- src/components/Layout/Navbar.vue
+++ src/components/Layout/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="bg-primary text-white py-6 px-16 w-full  shadow-lg z-50 fixed top-0  flex justify-between items-center">
+  <nav class="bg-primary text-white py-6 px-4 md:px-16 w-full  shadow-lg z-50 fixed top-0  flex justify-between items-center">
     <!-- Logo -->
 
       <h1 class="text-2xl font-bold">DevPulse</h1>
@@ -12,7 +12,7 @@
             {{homePage}}
           </router-link>
         </li>
-        <li><router-link to="/" class="hover:text-secondary font-bold ">Login</router-link></li>
+        <li><router-link to="/login" class="hover:text-secondary font-bold ">Login</router-link></li>
         <li><router-link to="/register" class="hover:bg-secondary  bg-accent py-2 px-3 rounded-lg  font-bold">Sign up</router-link></li>
 
         
@@ -26,21 +26,33 @@
       <!-- Mobile Sidebar -->
       <div v-if="isOpen" class="fixed inset-0 bg-black bg-opacity-50 z-40" @click="closeMenu"></div>
       
-      <div
-        class="fixed top-0 right-0 w-64 h-full bg-primary shadow-lg transform transition-transform duration-300 ease-in-out z-50"
-        :class="isOpen ? 'translate-x-0' : 'translate-x-full'"
-      >
-        <button @click="closeMenu" class="absolute top-4 right-4 text-white text-3xl focus:outline-none">
-          <i class="ri-close-line"></i>
-        </button>
+        <div
+          class="fixed top-0 right-0 w-full h-40 pt-4 bg-primary shadow-lg rounded-xl  transform transition-transform duration-300 ease-in-out z-50"
+          :class="isOpen ? 'translate-y-0 mt-24 ' : '-translate-y-full  '"
+        >
+          <button @click="closeMenu" class="absolute top-4 right-4 text-white text-3xl focus:outline-none">
+            <i class="ri-close-line"></i>
+          </button>
 
-        <ul class="mt-16 space-y-6 text-center">
-          <li><router-link to="/" @click="closeMenu" class="block py-2 hover:text-secondary">Home</router-link></li>
-          <li><router-link to="/about" @click="closeMenu" class="block py-2 hover:text-secondary">About</router-link></li>
-          <li><router-link to="/services" @click="closeMenu" class="block py-2 hover:text-secondary">Services</router-link></li>
-          <li><router-link to="/" @click="closeMenu" class="block py-2 hover:text-secondary">Contact</router-link></li>
-        </ul>
-      </div>
+          <ul class="space-y-6 text-center">
+            <li>
+              <router-link to="/" class="hover:text-secondary py-2 px-3 rounded-lg font-bold">
+                {{ homePage }}
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/login" @click="closeMenu" class="hover:text-secondary font-bold">
+                Login
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/register" @click="closeMenu" class="hover:bg-secondary bg-accent text-bold  py-4 px-12 rounded-lg font-bold">
+                Register
+              </router-link>
+            </li>
+          </ul>
+        </div>
+
   </nav>
 </template>
 
